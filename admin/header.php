5e59b681f946e379052eace14e35685a85e1d5ff
Francisco <franciscoagapitollarena@gmail.com>
2025-12-02 13:16:12 -0300

    adicionado pagina de usuários

---
+++ admin/header.php
@@ -0,0 +1,72 @@
+<?php
+ if (session_status() === PHP_SESSION_NONE) {
+     session_start();
+ }
+ $cartCount = 0;
+ if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+     foreach ($_SESSION['cart'] as $item) {
+         $cartCount += isset($item['quantidade']) ? (int)$item['quantidade'] : 0;
+     }
+ }
+ ?>
+
+  <div class="headerContainer">
+            <header>
+                <div class="logo">
+                  <h1>BASED</h1>
+                </div>
+                
+                <ul>
+              <li><a href="/projeto_programacao_web/index.php">Início</a></li>
+                <li><a href="/projeto_programacao_web/sobre.php">Sobre</a></li>
+                 <li><a href="/projeto_programacao_web/produtos.php">Produtos</a></li>
+                 <li><a href="/projeto_programacao_web/contato.php">Fale conosco</a></li>
+                  <li><a href="/projeto_programacao_web/admin/dashboard.php">Admin</a></li>
+
+
+                </ul>
+
+                <div class="iconLists">
+                    <i class="fa-solid fa-bars"></i>
+                </div>
+                <a href="/projeto_programacao_web/carrinho.php" class="cartLink">
+                    <div class="cart">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                        <?php if ($cartCount > 0): ?>
+                        <span class="cart-badge"><?php echo $cartCount; ?></span>
+                        <?php endif; ?>
+                    </div>
+                </a>
+
+                <div class="profile">
+                    <i class="fa-regular fa-user"></i>
+                </div>
+               
+            </header> 
+            <div class="blur"></div>
+            <div class="listsContainer">
+                 <ul>
+                    <li>Início</li>
+                    <li>Sobre</li>
+                    <li>Produtos</li>
+                    <li>Contato</li>
+                </ul>
+            </div>
+            <?php if (isset($_SESSION['user_nome'])): ?>
+            <div class="profileContainer">
+                    <ul>
+                        <li><?php echo htmlspecialchars($_SESSION['user_nome']); ?></li>
+                        <li><a href="/projeto_programacao_web/logout.php">Sair</a></li>
+                    </ul>
+                </div>
+            <?php else: ?>
+            <div class="profileContainer">
+                    <ul>
+                        <li><a href="/projeto_programacao_web/register.php">Registre-se</a></li>
+                        <li><a href="/projeto_programacao_web/login.php">Login</a></li>
+                    </ul>
+                </div>
+            <?php endif; ?>
+        </div>
+
+           <script src="../script.js"></script>
---
+++ admin/users.php
@@ -0,0 +1,81 @@
+<?php
+require_once "../conexao/connect.php";
+$sql = "SELECT * FROM users";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="../style.css?v=1.2">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <title>Usuarios Based</title>
+</head>
+
+<body>
+    <div class="all">
+        <?php include("../principais/header.php"); ?>
+        <div class="container">
+            <div class="textContainer">
+                <h1>Usuarios</h1>
+                <div class="gridContainer">
+                    <?php
+                    $resultado = mysqli_query($conn, $sql);
+
+                    if (mysqli_num_rows($resultado) > 0) {
+
+                        while ($contatos = mysqli_fetch_array($resultado)) {
+                            ?>
+                            <div class="box">
+
+                                <br>
+                                <h3><?= $contatos['id'] ?></h3>
+                                <br>
+                                <h3>Nome</h3>
+                                <span><?= $contatos['Nome'] ?></span>
+                                <br>
+                                <h3>Senha</h3>
+                                <span><?= $contatos['Senha'] ?></span>
+                                <br>
+                                <h3>Email</h3>
+                                <span><?= $contatos['email'] ?></span>
+                                <br>
+                                <H3>Endereço</H3>
+                                <span><?= $contatos['Endereço'] ?></span>
+                                <h3>Gastos</h3>
+                                <span><?= $contatos['gastos'] ?></span>
+                                <!-- <h1 class="price"><span>R$</span> <?= $contatos['price'] ?> </h1> -->
+
+
+                                <a style="width: 90%;" href="edit_mensagem.php?id=<?= $contatos['id'] ?>"><button
+                                        class="btnContainer">Editar</button></a>
+
+                                <br>
+
+                                <a style="width: 90%;" href="delete_mensagem.php?id=<?= $contatos['id'] ?>"
+                                    onclick="return confirm('Tem certeza que deseja deletar este produto?')">
+                                    <button class="btnContainer">Deletar</button>
+                                </a>
+
+                                <br>
+
+                                <a style="width: 90%;" href="add_produto.php"><button class="btnContainer">+ Adicionar Produto</button></a>
+                                <br>
+
+                            </div>
+                            <?php
+                        }
+                    }
+                    ?>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
